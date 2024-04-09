1468bdafb9a48110b70d26620b3cc6700c3677f9
Kasia Klepacka <kklepacka@henix.fr>
2024-04-09 12:08:30 +0200

    reporter modif

--- playwright.config.js
+++ playwright.config.js
@@ -22,8 +22,8 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: 'json',
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: { 
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://prestashop-nightly-cloud.acceptance.squashtest.org/',
